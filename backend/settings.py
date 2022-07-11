7259ca8fc83b788741d6c2a85e0c7436da1c5b44
Nils <github@anonymeanonymiker.de>
2022-07-11 23:24:05 +0200

    add more env vars

--- backend/settings.py
+++ backend/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for backend project.
 
@@ -75,11 +76,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.' + str(os.environ.get('DB_TYPE')),
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': int(os.environ.get('POSTGRES_PORT')),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
